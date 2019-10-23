88d919fc9256414ddd8c244ae43ccb396a4e4383
Brian <brianmcwilliams15@gmail.com>
2019-10-23 18:23:51 -0400

    Add mapview to map tab.

--- components/MAPS.js
+++ components/MAPS.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import MapView from 'react-native-maps';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
 
 //Page Carte
 class MAPS extends React.Component {
     render() {
       return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>MAPS</Text>
-
+          <MapView style={styles.mapStyle} />
         </View>
       );
     }  
 }
 
 export default MAPS
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
 
 //<Image
 //style={{width: 200, height: 200, marginBottom: 30}}
